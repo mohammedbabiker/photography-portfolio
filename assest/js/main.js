8827dbf269d49f347a6b269d30663ac55d5633e6
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-02 23:29:48 +0300

    fix formatting on html, css and javascript file

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,36 +1,44 @@
 // nav style on scroll
 window.addEventListener("scroll", () => {
-    document.querySelector("nav").classList.toggle("window-scrolled", window.scrollY > 0);
-})
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scrolled", window.scrollY > 0);
+});
 
 // contact button circle text style
 const textButtons = document.querySelectorAll(".contact__btn");
 
-textButtons.forEach(textButton => {
-    let text = textButton.querySelector("p");
+textButtons.forEach((textButton) => {
+  let text = textButton.querySelector("p");
 
-    text.innerHTML = text.innerHTML.split("").map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join("")
-})
+  text.innerHTML = text.innerHTML
+    .split("")
+    .map(
+      (character, index) =>
+        `<span style="transform: rotate(${index * 12}deg)">${character}</span>`
+    )
+    .join("");
+});
 
 // -------------------------gallery swiper------------------------
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    599: {
+      slidesPerView: 2,
+      spaceBetween: 40,
     },
-    breakpoints: {
-        599: {
-           slidesPerView: 2,
-           spaceBetween: 40,
-        },
-        1023: {
-          slidesPerView: 3,
-          spaceBetween: 60,
-        }   
-    }
-  });
+    1023: {
+      slidesPerView: 3,
+      spaceBetween: 60,
+    },
+  },
+});
 
 const nav = document.querySelector(".nav__links");
 const openNavBtn = document.querySelector("#nav__toggle-open");
@@ -40,7 +48,7 @@ const openNav = () => {
   nav.style.display = "flex";
   openNavBtn.style.display = "none";
   closeNavBtn.style.display = "inline-block";
-}
+};
 
 openNavBtn.addEventListener("click", openNav);
 
@@ -48,14 +56,12 @@ const closeNav = () => {
   nav.style.display = "none";
   openNavBtn.style.display = "inline-block";
   closeNavBtn.style.display = "none";
-}
+};
 
 closeNavBtn.addEventListener("click", closeNav);
-// close menu after clicking on links 
-if(document.body.clientWidth < 1024){
-  nav.querySelectorAll("li a").forEach(navLink =>{
+// close menu after clicking on links
+if (document.body.clientWidth < 1024) {
+  nav.querySelectorAll("li a").forEach((navLink) => {
     navLink.addEventListener("click", closeNav);
-  })
+  });
 }
-
-
